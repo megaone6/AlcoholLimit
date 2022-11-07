8f8e12c93cdc200d94c6bc5fc50eef5283be9d06
Your Name <you@example.com>
2022-11-07 21:54:34 +0000

    database layer created

---
+++ AlcoholLimit/AlcoholLimit/Data/ProfileItem.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlcoholLimit.Data
+{
+    public enum sex { WOMAN, MAN };
+    public class ProfileItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Weight { get; set; } //Size of the drink in milliliter (ml)
+        public sex Sex { get; set; }
+        public bool nightmode { get; set; }
+    }
+}

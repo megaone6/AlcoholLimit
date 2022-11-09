d3feacb3afe68b9d6d7ab56db7a0f840c6f8953a
Dóra László <65315172+megaone6@users.noreply.github.com>
2022-11-09 14:57:17 +0100

    Timer now works as intended

--- AlcoholLimit/AlcoholLimit/Pages/StartDrink.razor.cs
+++ AlcoholLimit/AlcoholLimit/Pages/StartDrink.razor.cs
@@ -2,28 +2,41 @@ namespace AlcoholLimit.Pages
 {
     public partial class StartDrink
     {
-        private const string STARTING_TIME = "00:00:00";
+        private const string DEFAULT_TIME = "00:00:00";
 
         private bool hideList = true;
         private bool isDrinking = false;
         private string drinkingStatus = "You have not started drinking yet.";
         private string buttonLabel = "Start drinking session";
         private double bloodAlcoholLevel = 0;
-        private string elapsedTime = STARTING_TIME;
+        private string elapsedTime = DEFAULT_TIME;
+        private System.Timers.Timer timer = new System.Timers.Timer(1);
+        private DateTime startTime = DateTime.Now;
+        private DateTime currentTime;
 
         private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
         {
-
-        }
-
-        protected override void OnInitialized()
-        {
-            startTimer();
+            InvokeAsync(() =>
+            {
+                currentTime = e.SignalTime;
+                elapsedTime = $"{currentTime.Subtract(startTime)}".Substring(0, 8);
+                StateHasChanged();
+            });
         }
 
         private void startTimer()
         {
+            startTime = DateTime.Now;
+            timer = new System.Timers.Timer(1);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
 
+        private void stopTimer()
+        {
+            timer.Enabled = false;
+            elapsedTime = DEFAULT_TIME;
         }
 
         private void changeStatus()
@@ -34,6 +47,7 @@ namespace AlcoholLimit.Pages
                 hideList = true;
                 drinkingStatus = "You have not started drinking yet.";
                 buttonLabel = "Start drinking session";
+                stopTimer();
             }
             else
             {
@@ -41,6 +55,7 @@ namespace AlcoholLimit.Pages
                 hideList = false;
                 drinkingStatus = "You have started drinking.";
                 buttonLabel = "Stop drinking session";
+                startTimer();
             }
         }
 

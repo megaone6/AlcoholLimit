5a20ffe8539406c38c002b81b7c6aff56f09d9c1
Balazs Fonyodi <fonyodi1balazs@gmail.com>
2022-11-20 21:47:09 +0100

    Merging BAC formula to Start drinking page (WIP cuz of timer)

--- AlcoholLimit/AlcoholLimit/Pages/StartDrink.razor.cs
+++ AlcoholLimit/AlcoholLimit/Pages/StartDrink.razor.cs
@@ -2,28 +2,65 @@ namespace AlcoholLimit.Pages
 {
     public partial class StartDrink
     {
-        private const string STARTING_TIME = "00:00:00";
+        private const string DEFAULT_TIME = "00:00:00";
 
         private bool hideList = true;
         private bool isDrinking = false;
         private string drinkingStatus = "You have not started drinking yet.";
         private string buttonLabel = "Start drinking session";
-        private double bloodAlcoholLevel = 0;
-        private string elapsedTime = STARTING_TIME;
+        private string elapsedTime = DEFAULT_TIME;
+        private System.Timers.Timer timer;
+        private System.Timers.Timer timer2;
+        private DateTime startTime = DateTime.Now;
+        private DateTime currentTime;
+        private double elapsedsomething = 0;
+        private int numberOfThirtyMins = 0;
+        TimeSpan elapsedSpan;
 
         private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
         {
+            InvokeAsync(() =>
+            {
+                currentTime = e.SignalTime.AddMinutes(29).AddSeconds(55);  //.Addminutes(minute) for testing
+                elapsedTime = $"{currentTime.Subtract(startTime)}".Substring(0, 8);
+                long elapsedTicks = currentTime.Ticks - startTime.Ticks;
+                TimeSpan elapsedSpan = new TimeSpan(elapsedTicks);
+                elapsedsomething = elapsedSpan.TotalMinutes;
 
+               /* if (elapsedsomething / (numberOfThirtyMins+1)*30 >= 1)
+                {
+                    numberOfThirtyMins++;
+                }*/
+                StateHasChanged();
+            });
+            
         }
-
-        protected override void OnInitialized()
+        private void OnTimedEvent2(Object source, System.Timers.ElapsedEventArgs e)
         {
-            startTimer(); //timer increment felorankent
+            InvokeAsync(() =>
+            {
+                displayBac = currentBloodAlcohol(true, 80, elapsedSpan.TotalHours, 0.014, 2);
+            });
         }
-
         private void startTimer()
         {
+            startTime = DateTime.Now;
+            timer = new System.Timers.Timer(1);
+            timer2 = new System.Timers.Timer(1);
+            timer.Elapsed += OnTimedEvent;
+            timer2.Elapsed += OnTimedEvent2;
+            timer2.Interval = 1800000;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+            timer2.AutoReset = true;
+            timer2.Enabled = true;
+        }
 
+        private void stopTimer()
+        {
+            timer.Enabled = false;
+            timer2.Enabled = false;
+            elapsedTime = DEFAULT_TIME;
         }
 
         private void changeStatus()
@@ -34,6 +71,7 @@ namespace AlcoholLimit.Pages
                 hideList = true;
                 drinkingStatus = "You have not started drinking yet.";
                 buttonLabel = "Start drinking session";
+                stopTimer();
             }
             else
             {
@@ -41,17 +79,33 @@ namespace AlcoholLimit.Pages
                 hideList = false;
                 drinkingStatus = "You have started drinking.";
                 buttonLabel = "Stop drinking session";
+                startTimer();
             }
         }
 
         private void addFoodOrDrink()
         {
-            //
+
         }
 
-        private void changeBloodAlcLevel()
+        public double displayBac;
+        public double currentBloodAlcohol(bool sex, int weight, double time, double alcoholInGrams, int numberOfDrinks)
         {
-            bloodAlcoholLevel = 0.1;
+            double alcoholMetabolization;
+            double r; //gender constant
+            if (sex == true)
+            {
+                r = 0.68;
+                alcoholMetabolization = 0.015;
+            }
+            else
+            {
+                r = 0.55;
+                alcoholMetabolization = 0.017;
+            }
+
+            double bloodAlcoholLevel = (numberOfDrinks * alcoholInGrams / (r * weight)) * 100 - (alcoholMetabolization * time);
+            return bloodAlcoholLevel;
         }
     }
 }

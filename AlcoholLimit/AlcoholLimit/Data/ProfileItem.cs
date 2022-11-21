efd79ab7b8831cc27d1cec5edbd38742488b361b
Your Name <you@example.com>
2022-11-21 13:12:47 +0000

    threshold added to profile

--- AlcoholLimit/AlcoholLimit/Data/ProfileItem.cs
+++ AlcoholLimit/AlcoholLimit/Data/ProfileItem.cs
@@ -10,6 +10,7 @@ namespace AlcoholLimit.Data
     public enum sex { WOMAN, MAN };
     public class ProfileItem
     {
+        [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
         public string Name { get; set; }
         public int Weight { get; set; }
@@ -17,5 +18,9 @@ namespace AlcoholLimit.Data
         public String Email { get; set; }
         public String Password { get; set; }
         public bool nightmode { get; set; }
+        public bool dailyNot { get; set; }
+        public bool weeklyNot { get; set; }
+        public bool thresholdNot { get; set; }
+        public float bloodThreshold { get; set; }
     }
 }

8b251cec2376c06e7c9d4bf3180ab2b74c4374d3
Dóra László <65315172+megaone6@users.noreply.github.com>
2022-11-30 15:17:52 +0100

    Added comments

--- AlcoholLimit/AlcoholLimit/Pages/StartDrink.razor.cs
+++ AlcoholLimit/AlcoholLimit/Pages/StartDrink.razor.cs
@@ -40,7 +40,7 @@ namespace AlcoholLimit.Pages
 
                 StateHasChanged();
             });
-            notifyOnHighBloodAlcohol(0.05);
+            notifyOnHighBloodAlcohol(0.05); // only here for testing purposes at the moment
         }
         private void startTimer()
         {
@@ -87,7 +87,7 @@ namespace AlcoholLimit.Pages
 
         private void addFoodOrDrink()
         {
-
+            // TODO: Implement adding drink to current session and notification when reaching the threshold
         }
 
         private void notifyOnHighBloodAlcohol(double threshold)

aaaf2933821464288d7b240ce7c357f96678bf66
Kiril <Kiril@DESKTOP-KA2I2A2>
2022-12-09 01:57:51 +0100

    Create new database for consumed drink items

---
+++ AlcoholLimit/AlcoholLimit/Data/ConsumedDrinkDatabase.cs
@@ -0,0 +1,31 @@
+﻿using AlcoholLimit.Utilities;
+
+namespace AlcoholLimit.Data
+{
+    public class ConsumedDrinkDatabase : Database<ConsumedDrinkItem>
+    {
+        #region Public methods
+
+        public override async Task<ConsumedDrinkItem> GetItemAsync(int id)
+        {
+            await Init();
+            return await database.Table<ConsumedDrinkItem>().Where(i => i.ID == id).FirstOrDefaultAsync();
+        }
+
+        public override async Task<ConsumedDrinkItem> GetItemAsync(string name)
+        {
+            return await database.Table<ConsumedDrinkItem>().Where(i => i.ID == 0).FirstOrDefaultAsync();
+        }
+
+        public override async Task<int> SaveItemAsync(ConsumedDrinkItem item)
+        {
+            await Init();
+            if (item.ID != 0)
+                return await database.UpdateAsync(item);
+            else
+                return await database.InsertAsync(item);
+        }
+
+        #endregion
+    }
+}
